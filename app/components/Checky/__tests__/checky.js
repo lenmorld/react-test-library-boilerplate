b015be1c2aa2345ed9891f7eb23bb3d019422a5e
Lenny LD <lenmorld@gmail.com>
2020-10-15 21:37:22 -0400

    add timeout to findBy*

--- app/components/Checky/__tests__/checky.js
+++ app/components/Checky/__tests__/checky.js
@@ -6,43 +6,49 @@ import '@testing-library/jest-dom'
 
 
 describe.only("Checky", () => {
-    beforeAll(() => {
-        jest.useFakeTimers();
-    })
+    describe('act + mock timers', () => {
+        beforeAll(() => {
+            jest.useFakeTimers();
+        })
 
-    afterAll(() => {
-        jest.useRealTimers()
-    })
+        afterAll(() => {
+            jest.useRealTimers()
+        })
 
-    it("updates state with delay - act() + mock timers", async () => {
-        act(() => {
+        it("updates state with delay - act() + mock timers", async () => {
+            act(() => {
+                render(<Checky />);
+            })
+
+            // screen.debug();
+            let label = screen.getByLabelText("false");
+            expect(label).toBeInTheDocument();
+
+            act(() => {
+                fireEvent.click(label);
+                jest.advanceTimersByTime(2000);
+            })
+
+            // screen.debug()
+            expect(screen.getByLabelText("true")).toBeInTheDocument();
+        });
+    });
+
+
+    describe('async utils', () => {
+        it("updates state with delay - RTL async utils", async () => {
             render(<Checky />);
-        })
 
-        // screen.debug();
-        let label = screen.getByLabelText("false");
-        expect(label).toBeInTheDocument();
+            let label = await screen.findByLabelText("false")
+            expect(label).toBeInTheDocument();
 
-        act(() => {
+            // screen.debug();
+
             fireEvent.click(label);
-            jest.advanceTimersByTime(2000);
-        })
 
-        // screen.debug()
-        expect(screen.getByLabelText("true")).toBeInTheDocument();
-    });
-
-    it("updates state with delay - RTL async utils", async () => {
-        render(<Checky />);
-
-        let label = await screen.findByLabelText("false")
-        expect(label).toBeInTheDocument();
-
-        // screen.debug();
-
-        fireEvent.click(label);
-
-        expect(await screen.findByLabelText("true")).toBeInTheDocument();
-        // screen.debug()
-    });
+            // expect(await screen.findByLabelText("true")).toBeInTheDocument();
+            expect(await screen.findByLabelText("true", {}, { timeout: 2000 })).toBeInTheDocument();
+            // screen.debug()
+        });
+    })
 });
--- app/components/Fetchy/__tests__/fetchy.js
+++ app/components/Fetchy/__tests__/fetchy.js
@@ -5,37 +5,43 @@ import '@testing-library/jest-dom'
 import Fetchy from "../Fetchy";
 
 describe("Fetchy", () => {
-    beforeAll(() => {
-        jest.useFakeTimers();
+    describe('act + mock timers', () => {
+        beforeAll(() => {
+            jest.useFakeTimers();
+        })
+
+        afterAll(() => {
+            jest.useRealTimers()
+        })
+
+        it("shows Loading and Data - mock timers", async () => {
+            act(() => {
+                render(<Fetchy />);
+            });
+
+            // screen.debug();
+            expect(screen.getByText("Loading")).toBeInTheDocument();
+
+            act(() => {
+                jest.advanceTimersByTime(3000);
+            });
+
+            // screen.debug();
+            expect(screen.getByText("Data:")).toBeInTheDocument();
+        });
     })
 
-    afterAll(() => {
-        jest.useRealTimers()
-    })
-
-    it("shows Loading and Data - mock timers", async () => {
-        act(() => {
+    describe('async utils', () => {
+        it("shows Loading and Data", async () => {
             render(<Fetchy />);
+
+            // expect(await screen.findByText("Loading")).toBeInTheDocument();
+            expect(await screen.findByText("Loading", {}, { timeout: 3000 })).toBeInTheDocument();
+            // screen.debug();
+
+            // expect(await screen.findByText("Data:")).toBeInTheDocument();
+            expect(await screen.findByText("Data:", {}, { timeout: 3000 })).toBeInTheDocument();
+            // screen.debug();
         });
-
-        // screen.debug();
-        expect(screen.getByText("Loading")).toBeInTheDocument();
-
-        act(() => {
-            jest.advanceTimersByTime(3000);
-        });
-
-        // screen.debug();
-        expect(screen.getByText("Data:")).toBeInTheDocument();
-    });
-
-    it("shows Loading and Data", async () => {
-        render(<Fetchy />);
-
-        expect(await screen.findByText("Loading")).toBeInTheDocument();
-        // screen.debug();
-
-        expect(await screen.findByText("Data:")).toBeInTheDocument();
-        // screen.debug();
     });
 });
